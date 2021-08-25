5a0433e3a9b1ed11adb911093824f7a9509ef8fd
github167 <catcat161-github@yahoo.com.hk>
2021-08-25 14:21:25 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+# https://github.com/dappuniversity/dbank
+npm install -g truffle
+https://github.com/leanthebean/puzzle-hunt
+
+
+git clone https://github.com/dappuniversity/dbank
+cd dbank
+npm install
+
+sed -i "s/development/develop/" truffle-config.js
